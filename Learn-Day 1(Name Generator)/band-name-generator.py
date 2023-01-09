47cbf11fc2d1426109cc218811f491e900674d23
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-01-09 09:02:22 +0545

    Day 1 Python

---
+++ Learn-Day 1(Name Generator)/band-name-generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.\n")
+city = input("What is the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be "+city+" "+pet_name)

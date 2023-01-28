ac7ad85abd4189b3a5937cf20d7dc7e1b822ff34
unknown <me.sangam.lamsal.10@gmail.com>
2023-01-28 10:50:20 +0545

    Day 25 Python

--- Learn-Day 25(CSV State Guess)/main.py
+++ Learn-Day 25(CSV State Guess)/main.py
@@ -37,10 +37,11 @@ while len(guessed_states) < 50:
     # screen.exitonclick()
 
     if answer == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [
+            state for state in all_states if state not in guessed_states]
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
